926ad4fd9e2d4f54178581b659ef65170eabce57
Jingyu <ojg1993@gmail.com>
2024-03-20 14:13:46 +0900

    reducer intro

---
+++ react_tutorial/onebite-react-3/src/App.jsx
@@ -0,0 +1,67 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import Exam from "./components/Exam";
+
+import { useState, useRef } from "react";
+
+const temp = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React tutorial",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Django tutorial",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "Network tutorial",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(temp);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Exam />
+      {/* <Header />
+      <Editor onCreate={onCreate} />
+      <List onUpdate={onUpdate} onDelete={onDelete} todos={todos} /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react_tutorial/onebite-react-3/src/components/List.jsx
@@ -0,0 +1,50 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    } else {
+      return todos.filter((todo) =>
+        todo.content.toLowerCase().includes(search.toLocaleLowerCase())
+      );
+    }
+  };
+
+  const filterdTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 💡</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="Input search keywrod"
+      />
+      <div className="todos_wrapper">
+        <div className="todos_wrapper">
+          {filterdTodos.map((todo) => {
+            return (
+              <TodoItem
+                key={todo.id}
+                {...todo}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
