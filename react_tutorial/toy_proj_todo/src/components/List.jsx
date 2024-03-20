524f5877496180dad7c02eb1723b2735f4a65b55
Jingyu <ojg1993@gmail.com>
2024-03-20 13:39:42 +0900

    todo update & delete

--- react_tutorial/toy_proj_todo/src/components/List.jsx
+++ react_tutorial/toy_proj_todo/src/components/List.jsx
@@ -2,7 +2,7 @@ import "./List.css";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -32,7 +32,14 @@ const List = ({ todos }) => {
       <div className="todos_wrapper">
         <div className="todos_wrapper">
           {filterdTodos.map((todo) => {
-            return <TodoItem key={todo.id} {...todo} />;
+            return (
+              <TodoItem
+                key={todo.id}
+                {...todo}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+              />
+            );
           })}
         </div>
       </div>
--- react_tutorial/toy_proj_todo/src/components/TodoItem.jsx
+++ react_tutorial/toy_proj_todo/src/components/TodoItem.jsx
@@ -1,12 +1,20 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDelBtn = () => {
+    onDelete(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input readOnly checked={isDone} type="checkbox" />
+      <input onChange={onChangeCheckBox} checked={isDone} type="checkbox" />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
-      <button>Delete</button>
+      <button onClick={onClickDelBtn}>Delete</button>
     </div>
   );
 };
